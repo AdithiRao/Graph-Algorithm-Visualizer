673d7cb957f89d17ccc6252f5c296aa1b87628e3
Adithi Rao <adigrao2012@gmail.com>
2020-07-15 02:25:58 -0400

    Updated bfs to search one box at a time

--- bfs.py
+++ bfs.py
@@ -15,35 +15,33 @@ class BFS:
         self.order_visited = []
         self.queue = deque()
         self.queue.append(start)
-        self.VISITED_1_STEP_AGO = []
-        self.VISITED_2_STEPS_AGO = []
-        self.VISITED_3_STEPS_AGO = []
-        self.VISITED_A_WHILE_AGO = []
+        self.VISITED_1_STEP_AGO = None
+        self.VISITED_2_STEPS_AGO = None
+        self.VISITED_3_STEPS_AGO = None
+        self.VISITED_A_WHILE_AGO = None
         self.target = target
         self.grid = grid
 
     def grid_updates(self):
         if self.VISITED_A_WHILE_AGO:
-            for (row, col) in self.VISITED_A_WHILE_AGO:
-                self.grid[row][col] = VISITED_A_WHILE_AGO
+            (row, col) = self.VISITED_A_WHILE_AGO
+            self.grid[row][col] = VISITED_A_WHILE_AGO
         if self.VISITED_3_STEPS_AGO:
-            for (row, col) in self.VISITED_3_STEPS_AGO:
-                self.grid[row][col] = VISITED_3_STEPS_AGO
-            self.VISITED_A_WHILE_AGO = deepcopy(self.VISITED_3_STEPS_AGO)
+            (row, col) = self.VISITED_3_STEPS_AGO
+            self.grid[row][col] = VISITED_3_STEPS_AGO
+            self.VISITED_A_WHILE_AGO = self.VISITED_3_STEPS_AGO
         if self.VISITED_2_STEPS_AGO:
-            for (row, col) in self.VISITED_2_STEPS_AGO:
-                self.grid[row][col] = VISITED_2_STEPS_AGO
-            self.VISITED_3_STEPS_AGO = deepcopy(self.VISITED_2_STEPS_AGO)
+            (row, col) = self.VISITED_2_STEPS_AGO
+            self.grid[row][col] = VISITED_2_STEPS_AGO
+            self.VISITED_3_STEPS_AGO = self.VISITED_2_STEPS_AGO
         if self.VISITED_1_STEP_AGO:
-            for (row, col) in self.VISITED_1_STEP_AGO:
-                self.grid[row][col] = VISITED_1_STEP_AGO
-            self.VISITED_2_STEPS_AGO = deepcopy(self.VISITED_1_STEP_AGO)
+            (row, col) = self.VISITED_1_STEP_AGO
+            self.grid[row][col] = VISITED_1_STEP_AGO
+            self.VISITED_2_STEPS_AGO = self.VISITED_1_STEP_AGO
         if self.queue:
-            self.VISITED_1_STEP_AGO = []
-            for (row, col) in self.queue:
-                if (row, col) not in self.visited_set:
-                    self.grid[row][col] = CURR_VISITING
-                    self.VISITED_1_STEP_AGO.append((row, col))
+            (row, col) = self.queue[0]
+            self.grid[row][col] = CURR_VISITING
+            self.VISITED_1_STEP_AGO = self.queue[0]
 
     def one_step(self):
         grid_height = len(self.grid)
@@ -51,24 +49,19 @@ class BFS:
         if len(self.visited_set) == grid_height*grid_width:
             return (False, True)
         while self.queue:
-            num_elements = len(self.queue)
+            curr_row, curr_col = self.queue[0]
+            if self.queue[0] == self.target:
+                self.grid[curr_row][curr_col] = FOUND
+                return (True, True)
             self.grid_updates()
-            while num_elements > 0:
-                curr_row, curr_col = self.queue.popleft()
-                if (curr_row, curr_col) in self.visited_set:
-                    num_elements -= 1
-                    continue
-                if (curr_row, curr_col) == self.target:
-                    self.grid[curr_row][curr_col] = FOUND
-                    return (True, True)
+            self.queue.popleft()
 
-                self.visited_set.add((curr_row, curr_col))
-                self.order_visited.append((curr_row, curr_col))
-                for dir in self.directions:
-                    if curr_row+dir[0] >= 0 and curr_row+dir[0] < grid_width and \
-                    curr_col+dir[1] >= 0 and curr_col+dir[1] < grid_height:
-                        if (curr_row+dir[0], curr_col+dir[1]) not in self.visited_set:
-                            self.queue.append((curr_row+dir[0], curr_col+dir[1]))
-                num_elements -= 1
+            self.order_visited.append((curr_row, curr_col))
+            for dir in self.directions:
+                if curr_row+dir[0] >= 0 and curr_row+dir[0] < grid_width and \
+                curr_col+dir[1] >= 0 and curr_col+dir[1] < grid_height:
+                    if (curr_row+dir[0], curr_col+dir[1]) not in self.visited_set:
+                        self.queue.append((curr_row+dir[0], curr_col+dir[1]))
+                        self.visited_set.add((curr_row+dir[0], curr_col+dir[1]))
             break
         return (False, False)
